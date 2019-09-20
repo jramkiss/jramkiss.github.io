c72ca137439aade19c2ef01bd39229361b45b943
Jonathan Ramkissoon <jonathanr@scribd.com>
2019-09-20 10:43:01 -0400

    slight css change

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "pixyll2-20190919172949";
+var CACHE_NAME = "pixyll2-20190920104223";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?201909191729",
+        "/css/pixyll.css?201909201042",
         "/"
       ]);
     })
