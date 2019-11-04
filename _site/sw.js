1aecc6c9eaecadc972572734c8eb71b21ad3e2d0
Jonathan Ramkissoon <jonathanr@scribd.com>
2019-11-03 21:59:27 -0500

    added _site

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "pixyll2-20191103213340";
+var CACHE_NAME = "pixyll2-20191103215814";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?201911032133",
+        "/css/pixyll.css?201911032158",
         "/"
       ]);
     })
