1df08a93b558636e9436c51abffc520ebaa22c05
Jonathan Ramkissoon <jramkissoon@mtlmp-jramkis1.home>
2023-11-07 23:15:19 -0500

    updated pf post

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "pixyll2-20231105195826";
+var CACHE_NAME = "pixyll2-20231107231402";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?202311051958",
+        "/css/pixyll.css?202311072314",
         "/"
       ]);
     })
